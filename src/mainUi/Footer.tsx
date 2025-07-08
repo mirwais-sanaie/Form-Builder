fe0e69e10f7b1f110ea2b18fe4a1d181a1405ef4
Mirwais <mirwais.sanaie@gmail.com>
2025-07-09 00:16:51 +0430

    ...

--- src/mainUi/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="bg-background text-center py-5">
-      All rights reserved &copy; Mirwais sanaie 2025
-    </div>
-  );
-}
-
-export default Footer;
